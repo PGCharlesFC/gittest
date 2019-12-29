4139f9a8a08da348dd7bc7c73180f11494c05e60
Charles <developer_charles@163.com>
2019-12-29 18:01:44 +0800

    Initial commit

---
+++ hello/hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.6
+# -*- encoding: utf-8 -*-
+"""
+    @File    : hello.py
+    @Time    : 2019/12/29 下午5:44
+    @Author  : Charles
+    @Version : 1.0
+    @Desc    : None
+"""
+
+
+print("你好，世界！")
